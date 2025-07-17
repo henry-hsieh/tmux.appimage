e9ab63ee64489a5fbdb0df660a851b3f3cb78d2d
Henry Hsieh <r901042004@yahoo.com.tw>
2025-07-17 22:05:17 +0800

    ci: remove latest centos docker image

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,6 @@ root_dir=$(dirname $(dirname $(realpath $0)))
 
 install='echo ""'
 if [[ "$1" == "fedora:latest" ]]; then
-  install='dnf install util-linux -y'
+  install='dnf install util-linux util-linux-script -y'
 fi
 docker run --rm -v $root_dir:$root_dir -w $root_dir $1 bash -c "$install; script -q -e -c \"TERM=tmux-256color ./scripts/test_tmux.sh ./build/Tmux-x86_64.AppImage $(cat $root_dir/packages/tmux.yaml | grep version | sed 's/.\+:\s//g')\" /dev/null"
